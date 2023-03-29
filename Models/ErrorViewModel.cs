c717f67533b346d0750d55b54ea507d54c4e027e
alterax05 <miky.porcellato@gmail.com>
2023-03-29 18:27:01 +0200

    Aggiungere i file di progetto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Progetto_Questionario_Steelco.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
